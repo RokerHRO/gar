7d8fd9f996b5ebd371ce16b3e19c027a314b6415
Lars H. Rohwedder <rokerhro@github.com>
2025-03-15 21:25:19 +0100

    make rolling_hash configurable for line lenghts before breaks

--- src/rolling_hash.cc
+++ src/rolling_hash.cc
@@ -1,4 +1,4 @@
-#include <cstdint>
+#include "siso93.hh"
 
 #include <algorithm>
 #include <cstdio>
@@ -7,56 +7,15 @@
 #include <string>
 #include <string_view>
 
-
-const unsigned LineLengthMin = 80;
-const unsigned LineLengthMax = 4096;
-
-const unsigned LineLengthStretch = LineLengthMax - LineLengthMin;
-const uint64_t HashStep = UINT64_MAX / LineLengthStretch;
+#include <unistd.h> // for getopt()
 
 
-const char* const encoding_value =
-//	 0123456789abcdef.123456789abcdef
-	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[2]^/" // 0x .. 1x
-	"_!*#$%$'()*+,-./0123456789:;(=)?" // 2x .. 3x
-	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[!]^_" // 4x .. 5x
-	"`abcdefghijklmnopqrstuvwxyz{|}~?" // 6x .. 7x
-	"`abcdefghijklmnopqrstuvwxyz{|}~." // 8x .. 9x
-	"_!0#$%#'()*+,-./0123456789:;1=3?" // Ax .. Bx
-	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[4]^7" // Cx .. Dx
-	"`abcdefghijklmnopqrstuvwxyz{|}~!";// Ex .. Fx
 
-char encoding_plane[257] =
-//	 0123456789abcdef.123456789abcdef
-	"11111111111111111111111111111111" // 0x .. 1x
-	"00100010000000000000000000001010" // 2x .. 3x
-	"00000000000000000000000000001001" // 4x .. 5x
-	"00000000000000000000000000000001" // 6x .. 7x
-	"11111111111111111111111111111111" // 8x .. 9x
-	"22122212222222222222222222221212" // Ax .. Bx
-	"22222222222222222222222222221221" // Cx .. Dx
-	"22222222222222222222222222222221";// Ex .. Fx
-
-const char* const encoding_tag =
-//	 000111222
-//	 012012012
-	"`.^,JK|ST" // 00
-	"~@ELMNUVW" // 01
-	":GHOPQXYZ" // 02
-	"'abijkrst" // 10
-	"cdelmnuvw" // 11
-	"fghopqxyz" // 12
-	";129[]FI(" // 20
-	"345{}A)=#" // 21
-	"657BCD*$%";// 22
-
-constexpr
+static constexpr
 uint64_t Pow(uint64_t basis, unsigned exponent)
 {
 	return exponent ? Pow(basis, exponent-1) * basis : 1 ;
 }
-
-typedef uint8_t U8;
 
 
 struct RollingHash
@@ -136,74 +95,70 @@ private:
 };
 
 
-uint64_t break_hash(unsigned length)
+class LineBreaker
 {
-	if(length<=LineLengthMin) return 0;
-	if(length>=LineLengthMax) return ~uint64_t(0);
-	
-	return (length-LineLengthMin) * HashStep;
-}
-
-
-static const unsigned factors[4] = {27, 9, 3, 1};
-
-std::pair<std::string, std::string> encode_siso93(const std::string& s)
-{
-	std::string tag, data;
-	tag.reserve((s.size()+3)/4);
-	data.reserve(s.size());
-	
-	const size_t max_size = s.size() & ~3ull;
-	for(size_t o=0; o<max_size; o+=4)
+public:
+	LineBreaker(unsigned min_length, unsigned max_length)
+	: LineLengthMin(min_length)
+	, LineLengthMax(max_length)
+	, LineLengthStretch{LineLengthMax - LineLengthMin}
+	, HashStep{UINT64_MAX / LineLengthStretch}
 	{
-		const unsigned t = encoding_plane[ U8(s[o  ]) ]*27
-		                 + encoding_plane[ U8(s[o+1]) ]*9
-		                 + encoding_plane[ U8(s[o+2]) ]*3
-		                 + encoding_plane[ U8(s[o+3]) ];
+		fprintf(stderr, "# LineLen: %u ... %u\n HashStep: 0x%016llx.\n", LineLengthMin, LineLengthMax, (unsigned long long)HashStep);
 		
-		tag += encoding_tag[t];
-		data += encoding_value[ U8(s[o  ]) ];
-		data += encoding_value[ U8(s[o+1]) ];
-		data += encoding_value[ U8(s[o+2]) ];
-		data += encoding_value[ U8(s[o+3]) ];
-	}
-	
-	const unsigned* factor = factors;
-	if(max_size < s.size())
-	{
-		unsigned t = 0;
-		size_t o = max_size;
-		while(o < s.size())
+		for(unsigned u=LineLengthMin-5; u<LineLengthMax+5; ++u)
 		{
-			   t += encoding_plane[ U8(s[o]) ] * (*factor);
-			data += encoding_value[ U8(s[o]) ];
-			++o;
-			++factor;
+			fprintf(stderr, "# Len: %u : HashStep: 0x%016llx.\n", u, (unsigned long long)break_hash(u) );
 		}
-		tag += encoding_tag[t];
 	}
-	
-	return std::make_pair(std::move(tag), std::move(data));
-}
 
-int main()
+	uint64_t break_hash(unsigned length) const noexcept
+	{
+		if(length<=LineLengthMin) return 0;
+		if(length>=LineLengthMax) return ~uint64_t(0);
+		
+		return (length-LineLengthMin) * HashStep;
+	}
+
+private:
+	const unsigned LineLengthMin;
+	const unsigned LineLengthMax;
+	const unsigned LineLengthStretch;
+	const uint64_t HashStep;
+};
+
+
+int main(int argc, char** argv)
 {
-	for(unsigned u=0; u<256; ++u)
-	{
-		encoding_plane[u] -= '0';
-	}
-
 	const std::string es = "Banane";
+
+	int opt = -42;
+	unsigned min_len = 40;
+	unsigned max_len = 256;
 	
-	RollingHash rh(12, "H3V2pgvnSnKmBHRrurim9g=="); // just 16 random bytes base64-encoded.
-	
-	fprintf(stderr, "# LineLen: %u ... %u\n HashStep: 0x%016llx.\n", LineLengthMin, LineLengthMax, (unsigned long long)HashStep);
-	
-	for(unsigned u=LineLengthMin-5; u<LineLengthMax+5; ++u)
+	while( (opt = getopt(argc, argv, "s:l:")) != -1)
 	{
-		fprintf(stderr, "# Len: %u : HashStep: 0x%016llx.\n", u, (unsigned long long)break_hash(u) );
+		switch(opt)
+		{
+			case 's' : min_len = atoi(optarg); break;
+			case 'l' : max_len = atoi(optarg); break;
+			default:
+				fprintf(stderr, "Usage: %s [-s min_length] [-l max_length]\n", argv[0]);
+				return 1;
+		}
 	}
 	
+	if(min_len<8) min_len=8;
+	if(min_len>4096) min_len=4096;
+	
+	if(max_len<min_len) max_len = min_len;
+	if(max_len>65536) max_len = 65536;
+	
+	LineBreaker lb(min_len, max_len);
+
+	RollingHash rh(12, "H3V2pgvnSnKmBHRrurim9g=="); // just 16 random bytes base64-encoded.
+/*	
+*/
 	std::string line;
 	while(!feof(stdin))
 	{
@@ -214,7 +169,7 @@ int main()
 		const auto h = rh.add( c );
 		line += char(c);
 		
-		if( h <= break_hash(line.size()) )
+		if( h <= lb.break_hash(line.size()) )
 		{
 			const auto [tag,data] = encode_siso93(line);
 			printf("> %s\n%s\n", tag.c_str(), data.c_str());
