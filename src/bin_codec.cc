a7a61f91f67faaa343d33ce5b6c71243f6873590
Lars H. Rohwedder <rokerhro@github.com>
2025-04-13 16:45:33 +0200

    implement BinCodec::encode() via siso93_encode()

--- src/bin_codec.cc
+++ src/bin_codec.cc
@@ -1,21 +1,59 @@
 #include "bin_codec.hh"
-#include <string>
+#include "siso93.hh"
+
+static
+void append_siso93(std::string& output, const std::string& line)
+{
+	const auto [tag,data] = encode_siso93(line);
+	output += "> ";
+	output += tag;
+	output += '\n';
+	output += data;
+	output += '\n';
+}
+
 
 BinCodec::BinCodec(unsigned min_line_length, unsigned max_line_length)
-: hash{}
+: hash{12, "H3V2pgvnSnKmBHRrurim9g=="} // just 16 random bytes base64-encoded.
 , breaker{min_line_length, max_line_length}
 {}
 
 
 std::string BinCodec::encode(std::string_view input)
 {
-
+	std::string ret;
+	ret.reserve(input.size() * 1.1);
+	
+	std::string line;
+	for(char c : input)
+	{
+		const auto h = hash.add( c );
+		line += char(c);
+		
+		if( h <= breaker.break_hash(line.size()) )
+		{
+//			line_lengths += line.length();
+//			++lines;
+			append_siso93(ret, line);
+			line.clear();
+		}
+	}
+	
+	if(!line.empty())
+	{
+		append_siso93(ret, line);
+	}
+	
+	return ret;
 }
 
 
 std::string BinCodec::decode(std::string_view input)
 {
-
+	std::string ret;
+	
+	
+	return ret;
 }
 
 
